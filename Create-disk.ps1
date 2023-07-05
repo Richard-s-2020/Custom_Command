4a148726d8a4cae5228f2130ec596d05ff4b48f8
Richard-s-2020 <66278064+Richard-s-2020@users.noreply.github.com>
2023-07-05 16:09:34 +0200

    test

--- Create-disk.ps1
+++ Create-disk.ps1
@@ -1,23 +1,29 @@
 function Create-Disk {
     param (
-        [string]$CSV,
-        [string]$Letter,
-        [string]$disk,        
-        [ValidateSet("False", "True", "y", "t")]
-        [string]$Debug
+        [string]$Disk
     )
 
-        # Check if running as administrator
-        $isElevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    # Check if running as administrator
+    # This will self elevate the script with a UAC prompt since it needs to be run as an Administrator to function properly.
+    if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Write-Host "You didn't run this script as an Administrator. This script will self-elevate to run as an Administrator and continue."
+        Start-Sleep -Seconds 1
+        Write-Host "                                               3"
+        Start-Sleep -Seconds 1
+        Write-Host "                                               2"
+        Start-Sleep -Seconds 1
+        Write-Host "                                               1"
+        Start-Sleep -Seconds 1
 
-        if (-not $isElevated) {
-            # Restart the script with administrative privileges
-            $arguments = "-ExecutionPolicy Bypass -Command `"$function:Create-Disk -CSV `"C:\path\to\file.csv`" -Letter `"D`" -disk `"1`" -Debug `"True`"`""
-            Start-Process -FilePath "powershell.exe" -ArgumentList $arguments -Verb RunAs
-            return
-        }
-        
-    if ($CSV -eq "") {
-        Write-Host "Hello"
+        # Build the argument list for self-elevation
+        $command = "Import-Module C:\Users\Richa\OneDrive\Documenten\WindowsPowerShell\Scripts\functions\load_Function.ps1; Create-CSV_User -disk $disk"
+        $argument = "-NoProfile -ExecutionPolicy Bypass -Command `"$command`""
+
+        Start-Process -FilePath "powershell.exe" -ArgumentList $argument -Verb RunAs
+
+    }
+    else {
+        # Continue with the rest of your script
+        Start-Process -FilePath "diskpart.exe" -Verb RunAsAdmin
     }
 }
